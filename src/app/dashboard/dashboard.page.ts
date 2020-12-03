09dcd5e3aba3c52d72e64d8b6bd2321ef063807a
Deleon Simoni <deleon.simoni@gmail.com>
2020-12-03 14:17:43 -0300

    alteração graficos

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -10,10 +10,13 @@ export class DashboardPage implements OnInit {
   @ViewChild("barCanvas") barCanvas: ElementRef;
   @ViewChild("doughnutCanvas") doughnutCanvas: ElementRef;
   @ViewChild("lineCanvas") lineCanvas: ElementRef;
+  @ViewChild("solicitacoesRefugio") solicitacoesRefugio: ElementRef;
 
+  
   private barChart: Chart;
   private doughnutChart: Chart;
   private lineChart: Chart;
+  private solicitacaoRefugio: Chart;
 
   constructor() {}
 
@@ -25,18 +28,19 @@ export class DashboardPage implements OnInit {
     this.barChart = new Chart(this.barCanvas.nativeElement, {
       type: "bar",
       data: {
-        labels: ["Angola", "Sutão", "Bolivia", "Síria", "Venezuela", "Congo"],
+        labels: ["Síria", "Bangladesh", "Angola", "Cuba", "Senegal", "Haiti", "Venezuela"],
         datasets: [
           {
-            label: "Total de",
-            data: [12, 19, 3, 5, 2, 3],
+            label: "Total",
+            data: [3, 2, 5, 5, 10, 25, 52],
             backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
               "rgba(255, 206, 86, 0.2)",
               "rgba(75, 192, 192, 0.2)",
               "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(255, 120, 64, 0.2)"
             ],
             borderColor: [
               "rgba(255,99,132,1)",
@@ -44,7 +48,8 @@ export class DashboardPage implements OnInit {
               "rgba(255, 206, 86, 1)",
               "rgba(75, 192, 192, 1)",
               "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
+              "rgba(255, 159, 64, 1)",
+              "rgba(255, 120, 64, 0.2)"
             ],
             borderWidth: 1
           }
@@ -66,7 +71,7 @@ export class DashboardPage implements OnInit {
     this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
       type: "doughnut",
       data: {
-        labels: ["Fuga", "Desemprego", "Politica", "Ameaças", "Escolaridade", "Família"],
+        labels: ["Raça", "Religião", "Nacionalidade", "Grupo Social", "Opinião Política", "Ambiental"],
         datasets: [
           {
             label: "Motivo do refugio",
@@ -115,6 +120,43 @@ export class DashboardPage implements OnInit {
         ]
       }
     });
+
+
+    this.solicitacaoRefugio = new Chart(this.solicitacoesRefugio.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"],
+        datasets: [
+          {
+            label: "Quantidade de refugiados no país",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [3538, 4282, 17631, 28385, 28670, 10308, 338666, 80057],
+            spanGaps: false
+          }
+        ]
+      }
+    });
+
+
+
+
+
   }
 
 }
